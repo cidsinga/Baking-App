3ad89c2b445636513c55fc80a7e8fce0f0d09e99
Casey Idsinga and Marcus Jensen <casey@>
2019-10-21 11:13:37 -0700

    build function that converts gallons to liters

---
+++ scripts.js
@@ -0,0 +1,38 @@
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+//
+var number1 = parseInt(prompt("Enter number of gallons"));
+//var number2 = parseInt(prompt("Enter your height (m)"));
+//
+// // alert(add(number1, number2));
+//
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+//
+// var multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
+//
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
+//
+// alert(multiply(number1, number2));
+
+// var bmi = function(){
+//
+//   var squareHeight = number2 * number2;
+//   var result = number1 / squareHeight;
+//   alert(result);
+// };
+//
+// bmi();
+
+var convertLiters = function(){
+  var liters = number1 * 3.78541;
+  alert(liters);
+
+}
+convertLiters();
